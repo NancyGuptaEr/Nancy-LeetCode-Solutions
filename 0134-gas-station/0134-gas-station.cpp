41c9ae4dd6c50be50d3d4bb5dbbad1cf5fe8deb9
Nancy Gupta <nancyguptaer@gmail.com>
2025-01-08 18:56:08 -0500

    Time: 106 ms (5.1%), Space: 130.5 MB (5.11%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        //TC = O(n), SC = O(1)
+        int deficit = 0;
+        int start = 0;
+        int balance = 0;
+        int n = gas.size();
+        for(int i = 0; i < n; i++)  {
+            balance += gas[i] - cost[i];
+            if(balance < 0) {
+                start = i+1;
+                deficit += balance;
+                balance = 0;
+            }
+        }
+        if(deficit + balance >= 0)   {
+            return start;
+        }
+        else    {
+            return -1;
+        }
+    }
+};
