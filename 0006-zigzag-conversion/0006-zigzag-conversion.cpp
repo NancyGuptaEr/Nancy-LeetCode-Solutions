34b39163e93dd5597a8da17956964bdcfb4b7e08
Nancy Gupta <nancyguptaer@gmail.com>
2025-01-08 19:21:03 -0500

    Time: 14 ms (34.54%), Space: 13.9 MB (67.06%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        //TC = O(n), SC = O(n)
+        if(numRows <= 1)
+            return s;
+        
+        vector<string>v(numRows, "");
+        int j = 0, dir = -1;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(j == numRows-1 || j == 0)
+                dir *= -1;
+            v[j] += s[i];
+            if(dir == 1)
+                j++;
+            else
+                j--;
+        }
+        string res;
+        for(auto &it:v) {
+            res += it;
+        }
+        return res;
+    }
+};
