ab0ba82d75898789c014252f6ff28b9823069e7c
Nancy Gupta <nancyguptaer@gmail.com>
2025-01-19 17:28:59 -0500

    Time: 0 ms (100%), Space: 21.7 MB (87.42%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        //TC = O(1), SC = O(1)
         const int num = 9;
         // Initialize boolean arrays to track digits in each row, column, and subgrid
         bool row[num][num] = {false};
@@ -14,13 +15,13 @@ public:
                 if(board[r][c] == '.')  {
                     continue;   // if not a number, proceed to the next cell
                 }
-                // Convert character digit to numerical index
+                // Convert character digit to numerical index (from 0 to 8)
                 int idx = board[r][c] - '0' - 1;
                 // Determine the subgrid number based on the current cell's position
                 int gridNum = (r/3) * 3 + (c/3);
 
                 // Check if the number already exists in the corresponding row, column, or subgrid
-                if(row[r][idx] ||col[c][idx] || subGrid[gridNum][idx])  {
+                if(row[r][idx] || col[c][idx] || subGrid[gridNum][idx])  {
                     return false;
                 }
                 // Update the boolean arrays for the current row, column, and subgrid with the current digit
